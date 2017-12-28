07bfc32d49ab0ac3f480453d8b5fe197fe4080b4
Your Name <you@example.com>
2017-12-27 23:16:57 -0800

    add directory read to undistort_fisheye

--- undistort_fisheye.py
+++ undistort_fisheye.py
@@ -11,11 +11,9 @@ import sys
 import os
 
 # DEBUGGING - manually enter matrix and distortion
-DIM = (1920, 1200)
+DIM = (1200, 1200)
 K = np.array([[726.49727255,0,945.7804803],[0,727.06050889,603.13960861],[0,0,1]])
 D = np.array([[-0.03016212],[-0.02889801],[0.0337639],[-0.0153955]])
-#print mtx
-#print dist
 
 # get intrinsics and distortion from input file
 #input_file = sys.argv[1]
@@ -43,14 +41,16 @@ D = np.array([[-0.03016212],[-0.02889801],[0.0337639],[-0.0153955]])
 #    row5 = line[2:end]                  # line 6
 #    d1, d2, d3, d4, d5 = row5.split(',')
 
-# populate intrinsic and distortion matrix
-#mtx = numpy.matrix([[float(m11),float(m12),float(m13)],[float(m21),float(m22),float(m23)],[0,0,1]])
-#dist = numpy.matrix([float(d1),float(d2),float(d3),float(d4),float(d5)])
 
-#print "intrinsic matrix: ", mtx
-#print "distortion coefficients: ", dist
+# define list of filepaths for processing
+filepath_list = []
+directory = sys.argv[1]
+filename_list = sorted(os.listdir(directory))
+for file in filename_list:
+    filepath_list.append( os.path.join(directory + file) )
 
 def undistort(img_path):
+    print img_path
     # read distorted image
     img = cv2.imread(img_path)
     h,w = img.shape[:2]
@@ -58,10 +58,10 @@ def undistort(img_path):
     map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, DIM, cv2.CV_16SC2)
     undistorted_img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
     cv2.imshow("undistorted", undistorted_img)
-    cv2.imwrite("undistorted1.png",undistorted_img)
+    cv2.imwrite("undistorted_%i",undistorted_img)
     cv2.waitKey(1000)
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    for p in sys.argv[1:]:
-        undistort(p)
+    for fname in filepath_list:
+        undistort(fname)
