1d6a185bc957e3d119a14233711a00a4819eea67
Ben <bdjohnson529@gmail.com>
2018-01-29 00:50:07 -0800

    began work on oksi stitcher

---
+++ stitching/oksi-stitcher.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include "/home/ben/opencv_toolbox/stitching/stitch-find/oksi_stitcher.hpp"
+
+using namespace std;
+using namespace cv;
+
+
+int main(int argc, char* argv[])
+{
+	OKSI_Stitcher Stitcher;
+
+	return 1;
+}
+
+
+
