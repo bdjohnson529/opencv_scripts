806492a3caf0ebbf69728687ea8c1613798b1f31
Ben Johnson <ben@oksi.com>
2018-02-03 19:44:02 -0800

    began work on stitching

---
+++ stitching/source/oksi_stitcher.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include "oksi_stitcher.hpp"
+
+using namespace std;
+using namespace cv;
+
+
+int main(int argc, char* argv[])
+{
+	printf("test output");
+
+	return 1;
+}
+
+
+
